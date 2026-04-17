6fffca26f975aa4aa19c4c5f1d26bed918dcb77d
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-04-17 16:14:59 -0500

    feat: Create editing state and input

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,13 +1,53 @@
-export default function Summary() {
-    return (
-        <section className="main-section-wrapper">
-            <div className="heading-wrapper">
-                <h3>Personal Summary</h3>
-                <button className="edit-button">
+import { useState } from "react";
+
+export default function Summary({userInfo, setUserInfo}) {
+  const [editFlag, setEditFlag] = useState(false);
+
+  function handleSummaryChange(e) {
+    setUserInfo({
+        ...userInfo,
+        summary: e.target.value
+    })
+  }
+
+  function enableEdit() {
+    if (editFlag === false) {
+      setEditFlag(true);
+    } else setEditFlag(false)
+  }
+
+  function submitChanges() {
+    if (editFlag === true) {
+      setEditFlag(false)
+    } else setEditFlag(true)
+  }
+
+  return (
+    <section className="main-section-wrapper">
+        <div className="heading-wrapper">
+            <h3>Personal Summary</h3>
+            {!editFlag && 
+                <button aria-label="edit-button" type="button" className="edit-button" onClick={enableEdit}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#808080"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
                 </button>
+            }
+        </div>
+        {!editFlag ? (
+            <div>
+                <p>{userInfo.summary}</p>
             </div>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-        </section>
+        ) : (
+            <div className="editing">
+                <textarea
+                    value={userInfo.summary}
+                    className="edit-summary-field"
+                    onChange={handleSummaryChange}>
+                </textarea>
+                <button type="button" className="submit editing-button" onClick={submitChanges}>
+                    Submit
+                </button>
+            </div>
+        )}
+    </section>
     )
 }
