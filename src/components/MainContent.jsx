36710abe9eb721be9487ee98ca05fc0b0b7c2b11
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-04-17 16:14:40 -0500

    feat: Pass userInfo props to child components

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,11 @@
 import Experience from "./Experience.jsx";
 import Summary from "./Summary.jsx";
 
-export default function MainContent () {
+export default function MainContent ({userInfo, setUserInfo}) {
     return (
         <section className="main-content-wrapper">
-            <Summary />
-            <Experience />
+            <Summary userInfo={userInfo} setUserInfo={setUserInfo}/>
+            <Experience userInfo={userInfo} setUserInfo={setUserInfo}/>
         </section>
     )
 }
