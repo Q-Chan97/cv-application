ec26808848a4ab7dad85e5b3172e157a96c88786
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-04-20 13:36:21 -0500

    Remove unnecessary file

--- src/components/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-    return (
-        <>
-            <p>000-000-0000</p>
-            <p>ExampleEmail@domain.com</p>
-            <p>New York City, NY</p>
-        </>
-    )
-}
