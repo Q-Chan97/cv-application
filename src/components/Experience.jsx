f3e7c560614173ae5446d0179dcd030d11d4a7ba
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-04-20 16:27:15 -0500

    feat: Delegate job info to own component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,40 +1,15 @@
-export default function Experience() {
+import JobArticle from "./Job.jsx"
+
+export default function Experience({userInfo, setUserInfo}) {
     return (
         <section className="main-section-wrapper">
             <div className="heading-wrapper">
                 <h3>Professional Experience</h3>
             </div>
             <div className="job-section-wrapper">
-                <article className="job-wrapper">
-                    <h4>Bolonzo Incorporated</h4>
-                    <h5>Head Bullywogger</h5>
-                    <h5>May 2023 - April 2026</h5>
-                    <ul>
-                        <li>Coordinated several applications</li>
-                        <li>Plunged into the depths of Hades itself</li>
-                        <li>Infiltrated international government organizations</li>
-                    </ul>
-                </article>
-                <article className="job-wrapper">
-                    <h4>Trebonius Enterprises</h4>
-                    <h5>Junior Bollywogger</h5>
-                    <h5>March 2020 - May 2023</h5>
-                    <ul>
-                        <li>Developed internal systems</li>
-                        <li>Managed office correspondence</li>
-                        <li>Delivered coffee in a timely manner</li>
-                    </ul>
-                </article>
-                <article className="job-wrapper">
-                    <h4>Pizza Palace</h4>
-                    <h5>Tool Guy</h5>
-                    <h5>June 2019 - March 2020</h5>
-                    <ul>
-                        <li>Handled grease catchers</li>
-                        <li>Put out that one fire pretty well</li>
-                        <li>At least I think so, don't ask Jeff about it</li>
-                    </ul>
-                </article>
+                {userInfo.jobs.map((job, index) => (
+                    <JobArticle key={index} jobIndex={index} companyName={job.companyName} title={job.title} dates={job.dates} duties={job.duties} userInfo={userInfo} setUserInfo={setUserInfo}/>
+                ))}
             </div>
         </section>
     )
